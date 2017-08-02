7edbd0f44287078a064b10fbc052634956ed3269
Sahil Mantha <sahilmantha@Rameshs-MacBook-Pro.local>
2017-08-01 22:54:09 -0700

    Session 4 Loop Homework

--- ForLoopLesson/src/Session4LoopHomework.java
+++ ForLoopLesson/src/Session4LoopHomework.java
@@ -4,10 +4,17 @@ public class Session4LoopHomework {
 	public static void main(String[] args) {
 		
 		for(int i = 5; i <= 100; i = i + 10){
-			System.out.print(i+",");
+			if(i <90) {
+				System.out.print(i+",");
+			}else {
+				System.out.print(i);
+			}
+			
+			
 		}
 	
 
 	}
 
 }
+
