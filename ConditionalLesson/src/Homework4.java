72f45d78794ac6b945aebc2d45cdbb85bfb6a077
Sahil Mantha <sahilmantha@Rameshs-MacBook-Pro.local>
2017-08-01 22:18:35 -0700

    Session 4 Homework

---
+++ ConditionalLesson/src/Homework4.java
@@ -0,0 +1,19 @@
+
+public class Homework4 {
+
+	public static void main(String[] args) {
+	
+		int age = 87;
+		if(age < 13) {
+			System.out.println("You are a kid");
+		}else if (age >=13 && age <=19) {
+			System.out.println("You are a teen");
+		}else if (age >=20 && age <=60) {
+			System.out.println("You are an adult");
+		}else if(age> 60) {
+			System.out.println("You are a senior citizen");
+		}
+		
+
+	}
+}
