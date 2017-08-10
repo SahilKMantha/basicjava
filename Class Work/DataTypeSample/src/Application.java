4b9242ca15dc9d7bbc08e45448ca6c72b78f2dd3
Sahil Mantha <sahilmantha@Rameshs-MacBook-Pro.local>
2017-08-09 21:27:07 -0700

    Organized Files

---
+++ Class Work/DataTypeSample/src/Application.java
@@ -0,0 +1,27 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Learning my own data types");
+
+	Person p1 = new Person();
+		p1.age = 14;
+		p1.name = "Sahil";
+		p1.grade = 9;
+		
+		System.out.println(p1.name);
+		System.out.println(p1.age);
+		
+	Person p2 = new Person();
+		p2.age = 10;
+		p2.name = "Nikhil";
+		p2.grade = 5;
+		
+		System.out.println(p2.name);
+		System.out.println(p2.age);
+	
+}
+}
+ // IDE = Interactive Development Environment (Eclipse)
+// "x" = constant (cannot change)
+// x = variable (can change)
---
+++ Class Work/MyCoolDataTypeSample/src/Application.java
@@ -0,0 +1,17 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Car c1 = new Car();
+			c1.company = "BMW";
+			c1.name = "I3";
+			c1.year = 2016;
+		
+		System.out.println(c1.company);
+		System.out.println(c1.name);
+		System.out.println(c1.year);
+}
+
+}
