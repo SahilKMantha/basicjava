38cc5aff4d58a649f98fdfe46fcaafcd03925fc5
Sahil Mantha <sahilmantha@Rameshs-MacBook-Pro.local>
2017-08-15 20:55:22 -0700

    Homework

--- Homework/MyHomeWorkDataSample/src/Application.java
+++ Homework/MyHomeWorkDataSample/src/Application.java
@@ -3,7 +3,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		Car c1 = new Car();
-			c1.Model = "Civic";
+			c1.Model = "Camry";
 			c1.Company = "Toyota";
 			c1.year = 2017;
 			c1.Tires = "Bridgestone";
@@ -13,7 +13,11 @@ public class Application {
 			System.out.println(c1.year);
 			System.out.println(c1.Tires);
 			
-
+		Car c2 = new Car();
+			c2.Model = "Civic";
+			c2.Company = "Honda";
+			
+			System.out.println(c2.Model);
 	}
 
 }
