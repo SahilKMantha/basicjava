12e73a6811d034027ac6815d2f6a35663fa6fed3
Sahil Mantha <sahilmantha@Rameshs-MacBook-Pro.local>
2017-09-04 18:30:55 -0700

    Homework

---
+++ Homework/CarSample/src/Application.java
@@ -0,0 +1,13 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Car c = new Car();
+				c.start();
+				c.stop();
+	
+	}
+
+}
---
+++ Homework/CarSample/src/Car.java
@@ -0,0 +1,17 @@
+
+public class Car {
+
+	
+		void start(){
+			System.out.println("Start the Car");
+		}
+
+	
+		void stop(){
+			System.out.println("Stop the Car");
+		}
+	
+}
+
+
+
