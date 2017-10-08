426f24956bc2c6f11586f3de096e0d93bdd79dc4
Sahil Mantha <sahilmantha@Rameshs-MacBook-Pro.local>
2017-10-07 18:45:48 -0700

    Class Work

--- Class Work/Review2/src/Application.java
+++ Class Work/Review2/src/Application.java
@@ -2,11 +2,12 @@
 public class Application {
 
 	public static void main(String[] args) {
-	System.out.println("Hello World");
+	
      //Java is OOP - Object Oriented Program - Everything stays within the class
 	
-	String name = "Sahil";
-	int d = 0;
+	
+	int age = 14;
+	int d = 47;
 	
 	/*for(; d<=10 ; d++ ) {
 		System.out.println("helloworld");*/
@@ -14,7 +15,7 @@ public class Application {
 	Person p1 = new Person("Sahil");
 	Person p2 = new Person("Ramesh");
 	p1.printDetails();
-	p2.printDetails();
+	p2.printDetails2();
 	
 		
 	
--- Class Work/Review2/src/Person.java
+++ Class Work/Review2/src/Person.java
@@ -2,10 +2,18 @@
 public class Person {
 
 	String name;
-	public void printDetails() {
-		System.out.println("Name =" + name);
+	int age;
+	int d;
+	Person(int inputage){
+		age = inputage;
 	}
 	
+	public void printDetails() {
+		System.out.println("Name = " + name + " and age = " + "14");
+	}
+	public void printDetails2() {
+		System.out.println("Name = " + name + " and age = " + "47");
+	}
 	 Person(String input) {	
 		name = input; 
 	}
