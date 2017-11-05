b460469f200a3c528940b2c1fb657812e7ae1607
Sahil Mantha <sahilmantha@Rameshs-MacBook-Pro.local>
2017-11-04 23:12:01 -0700

    stuff

---
+++ Homework/Session15HW/src/Password.java
@@ -0,0 +1,30 @@
+
+public class Password {
+
+	int age;
+	String name;
+
+	Password(int age) {
+		
+		this.age = age; 
+	}
+	
+	
+	private void pass () {
+		System.out.println("password");
+	}
+	
+	
+	
+	void fakepass () {
+		System.out.println("akfjjka fhkjah kaf");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Homework/Session15HW/src/SocialSecurity.java
@@ -0,0 +1,21 @@
+
+public class SocialSecurity {
+
+	int age;
+	String name;
+	
+	SocialSecurity(int age){
+	
+	this.age = age;
+	}
+	
+	private void number () {
+		System.out.println("11111111111111");
+	}
+	
+	void fakenumber () {
+		System.out.println("12312314143422132");
+	}
+	
+	
+}
